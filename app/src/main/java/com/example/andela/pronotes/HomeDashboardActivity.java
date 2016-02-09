6883db0b32ba4708d4cdaf40c6b090167172292b
andela <chidiebere.nwokocha@andela.com>
2016-02-09 02:13:01 +0100

    [Starts #113289103] Modify the existing view, which was generated automatically while starting the application

--- app/src/main/java/com/example/andela/pronotes/HomeDashboardActivity.java
+++ app/src/main/java/com/example/andela/pronotes/HomeDashboardActivity.java
@@ -23,14 +23,14 @@ public class HomeDashboardActivity extends AppCompatActivity
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             .setAction("Action", null).show();
       }
-    });
+    });*/
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -54,19 +54,13 @@ public class HomeDashboardActivity extends AppCompatActivity
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.home_dashboard, menu);
     return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
     if (id == R.id.action_settings) {
       return true;
     }
@@ -77,20 +71,23 @@ public class HomeDashboardActivity extends AppCompatActivity
   @SuppressWarnings("StatementWithEmptyBody")
   @Override
   public boolean onNavigationItemSelected(MenuItem item) {
-    // Handle navigation view item clicks here.
     int id = item.getItemId();
 
-    if (id == R.id.nav_camera) {
-      // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
+    if (id == R.id.nav_home) {
 
-    } else if (id == R.id.nav_slideshow) {
+    } else if (id == R.id.nav_reminder) {
 
-    } else if (id == R.id.nav_manage) {
+    } else if (id == R.id.nav_notebooks) {
+
+    } else if (id == R.id.nav_collection) {
+
+    } else if (id == R.id.nav_settings) {
 
     } else if (id == R.id.nav_share) {
 
-    } else if (id == R.id.nav_send) {
+    } else if (id == R.id.nav_sync) {
+
+    } else if (id == R.id.nav_trash) {
 
     }
 
@@ -98,4 +95,5 @@ public class HomeDashboardActivity extends AppCompatActivity
     drawer.closeDrawer(GravityCompat.START);
     return true;
   }
+
 }
