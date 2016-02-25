bfbbdf613442c015a0b7d8813d0d7fc2837ee44b
andela <chidiebere.nwokocha@andela.com>
2016-02-25 12:56:59 +0100

    [#113767647] Include interface FontManager which manages font type for reading note and listing notes

---
+++ app/src/main/java/com/example/andela/pronotes/utils/FontManager.java
@@ -0,0 +1,11 @@
+package com.example.andela.pronotes.utils;
+
+import android.widget.TextView;
+
+/**
+ * Created by andela on 2/25/16.
+ */
+public interface FontManager {
+
+   void setFontType(TextView view, String fonttype);
+}
