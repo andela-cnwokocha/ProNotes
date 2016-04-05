469aeaab33d01ce2e80eb4daad673e2c7308e3e7
andela-cnwokocha <chidiebere.nwokocha@andela.com>
2016-04-05 13:21:14 +0100

    Finish lock dialog for setting password

--- app/src/main/java/com/example/andela/pronotes/utils/LockNoteDialog.java
+++ app/src/main/java/com/example/andela/pronotes/utils/LockNoteDialog.java
@@ -1,11 +1,14 @@
 package com.example.andela.pronotes.utils;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -14,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -25,9 +29,11 @@ import com.example.andela.pronotes.R;
  * implements TextView.OnEditorActionListener
  * A simple {@link Fragment} subclass.
  */
-public class LockNoteDialog extends DialogFragment  {
+public class LockNoteDialog extends DialogFragment implements EditText.OnEditorActionListener {
   private EditText password;
   private ImageButton showIcon;
+  private Button okButton;
+  private Button cancelButton;
 
   public LockNoteDialog() {
     // Required empty public constructor
@@ -52,8 +58,25 @@ public class LockNoteDialog extends DialogFragment  {
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
+    okButton = (Button) view.findViewById(R.id.ok_button);
+    cancelButton = (Button) view.findViewById(R.id.cancel_button);
     password = (EditText) view.findViewById(R.id.edittext_lock);
     showIcon = (ImageButton) view.findViewById(R.id.password_icon);
+    password.setOnEditorActionListener(this);
+
+    cancelButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        dismiss();
+      }
+    });
+
+    okButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        getNotePassword();
+      }
+    });
 
     showIcon.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -76,8 +99,24 @@ public class LockNoteDialog extends DialogFragment  {
     getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
   }
 
+  @Override
+  public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+    if (EditorInfo.IME_ACTION_DONE == actionId) {
+      getNotePassword();
+      return true;
+    }
+    return false;
+  }
+
   public interface NoteLockDialogListener {
     void onFinishPasswordEntry(String password);
+  }
+
+  private void getNotePassword() {
+    NoteLockDialogListener listener = (NoteLockDialogListener) getActivity();
+    listener.onFinishPasswordEntry(password.getText().toString());
+    dismiss();
+    Log.i("Clickz", "Clicked");
   }
 
 }
